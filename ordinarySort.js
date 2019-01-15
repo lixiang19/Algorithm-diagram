3f53cdbedb3a3c97865ad866d9e50327c629c13d
lixiang19 <33660050+lixiang19@users.noreply.github.com>
2019-01-15 14:50:56 +0800

    新建

---
+++ ordinarySort.js
@@ -0,0 +1,25 @@
+// 普通排序从小到大
+function findSmallest(arr) {
+  let smallest = arr[0]
+  let smallestIndex = 0
+ for (let i = 1; i < arr.length; i++) {
+   const element = arr[i];
+   if (element<smallest) {
+     smallest = element
+     smallestIndex = i
+   }
+ }
+ return smallestIndex
+}
+function selectionSort(arr) {
+  let newArr = []
+  let length =arr.length
+  for (let i = 0; i < length; i++) {
+    smallestIndex = findSmallest(arr)
+    newArr.push(arr[smallestIndex])
+    arr.splice(smallestIndex,1)
+    console.log(arr)
+  }
+  return newArr
+}
+const arr1 = [1,5,6,4,8,9]
